8cd4bf0cef5b723d6c57cd5a4cb3ce98c4ccf952
SoumyaSC <soumya9242@gmail.com>
2020-08-21 12:44:47 +0530

    Commit 11 - added Codeforces problem, added modifications to code

--- src/Algorithms/DynamicProgramming/EqualSumPartition.java
+++ src/Algorithms/DynamicProgramming/EqualSumPartition.java
@@ -1,5 +1,13 @@
 package Algorithms.DynamicProgramming;
 
+/*Partition problem is to determine whether a given set can be partitioned
+into two subsets such that the sum of elements in both subsets is same.
+        Examples:
+
+        arr[] = {1, 5, 11, 5}
+        Output: true
+        The array can be partitioned as {1, 5, 5} and {11}*/
+
 public class EqualSumPartition {
 
     public int findSumOfArr(int[] arr){
